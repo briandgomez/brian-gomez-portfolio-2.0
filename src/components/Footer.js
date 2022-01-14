73090150ff8379cd2f891c54f9647102001669b8
briandgomez <briandgomez98@gmail.com>
2022-01-13 23:34:14 -0500

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div style={{margin: '50px 0px' }}>
+            <ul className="nav navbar">
+                <li>
+                <a href="https://github.com/briandgomez"><img src="https://img.icons8.com/color/48/000000/github--v1.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/brian-gomez-8a5a85211"><img src="https://img.icons8.com/color/48/000000/linkedin.png"/></a>
+                </li>
+                <li>
+                    <a href="https://stackoverflow.com/users/16557739/brian-gomez"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png"/></a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer;
