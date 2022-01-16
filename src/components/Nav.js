3ec488f0b95e7d6395f7f2cb3ca4f8476ecfcd56
briandgomez <briandgomez98@gmail.com>
2022-01-16 16:29:47 -0500

    highlighted text in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,23 +3,35 @@ import React from 'react';
 
 function NavBar(props) {
     const tabs = ['About Me', 'Contact', 'Portfolio', 'Resume'];
+
+    function MouseOver(event) {
+        event.target.onmouseover= 'collapse';
+    }
+
+    function MouseOut(event) {
+        event.target.onmouseout = "collapse.show";
+    }
+    
     return (
-        <ul className="nav navbar">
-            {tabs.map(currentTab => (
-                <li key={currentTab} >
-                    <a
-                        href={'#' + currentTab.toLowerCase()}
-                        onClick={() => props.handlePageChange(currentTab)}
-                        className={
-                            props.currentPage === currentTab ? `nav-link active bg-secondary` : 'nav-link bg-danger'
-                        }
-                    >
-                        {currentTab}
-                    </a>
-                </li>
-            ))
-            }
-        </ul >
+        <div>
+            <ul id="nav-bar" className="nav navbar" onMouseOver={MouseOver} onMouseOut={MouseOut}>
+                {tabs.map(currentTab => (
+                    <li key={currentTab} >
+                        <a
+                            href={'#' + currentTab.toLowerCase()}
+                            onClick={() => props.handlePageChange(currentTab)}
+                            className={
+
+                                props.currentPage === currentTab ? `nav-link active text-success` : 'nav-link'
+                            }
+                        >
+                            {currentTab}
+                        </a>
+                    </li>
+                ))
+                }
+            </ul >
+        </div>
     );
 }
 
